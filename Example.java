4ce182d3fd41901b67313bcaf029788163baab98
Roascro <rollycrompton@gmail.com>
2022-10-04 12:22:54 +0100

    Updated some words and worked out how git works

--- Example.java
+++ Example.java
@@ -6,6 +6,7 @@
 class Example  {
 	//A Java program begins with a call to main().
 	public static void main(String[] args) {
-		System.out.println("Java drives the web.");
+		System.out.println("Java drives the web, I guess.");
 	}
 }
+//I have in fact made changes brrr
